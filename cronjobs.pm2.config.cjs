d1568bb39abc827c93b85158d4fa8da3d07df5d5
MaxLiebsch <liebsch@dipmaxexport.com>
2024-06-09 09:05:28 +0200

    fix

--- cronjobs.pm2.config.cjs
+++ cronjobs.pm2.config.cjs
@@ -6,6 +6,7 @@ module.exports = {
       name: `cronjobs_${version}`,
       script: "/root/.nvm/versions/node/v20.11.1/bin/yarn",
       args: "--cwd '/root/cronjobs-arbitrage' start",
+      interpreter: "/root/.nvm/versions/node/v20.11.1/bin/node",
       env: {
         NODE_ENV: "production",
       },
