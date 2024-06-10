899a344f70101ec9558b7fd816cd7f55ef112eb7
MaxLiebsch <liebsch@dipmaxexport.com>
2024-06-10 12:54:59 +0200

    fix: update node version

--- cronjobs.pm2.config.cjs
+++ cronjobs.pm2.config.cjs
@@ -4,9 +4,9 @@ module.exports = {
   apps: [
     {
       name: `cronjobs_${version}`,
-      script: "/root/.nvm/versions/node/v20.11.1/bin/yarn",
+      script: "/root/.nvm/versions/node/v20.14.0/bin/yarn",
       args: "--cwd '/root/cronjobs-arbitrage' start",
-      interpreter: "/root/.nvm/versions/node/v20.11.1/bin/node",
+      interpreter: "/root/.nvm/versions/node/v20.14.0/bin/node",
       env: {
         NODE_ENV: "production",
       },
