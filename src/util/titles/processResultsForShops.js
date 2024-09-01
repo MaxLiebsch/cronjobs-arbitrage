b64f02ede5c00df4132e6a7bc80fa2ba06cd7de8
MaxLiebsch <liebsch@dipmaxexport.com>
2024-09-01 22:39:34 +0200

    fix: remove error

--- src/util/titles/processResultsForShops.js
+++ src/util/titles/processResultsForShops.js
@@ -42,106 +42,105 @@ export const processResultsForShops = async (fileContents, batchData) => {
         .find({ _id: { $in: ids.map((id) => new ObjectId(id)) } })
         .toArray();
 
-      if (!products.length)
-        throw new Error("No products found for batch " + batchId);
+      if (products.length) {
+        for (let index = 0; index < results.length; index++) {
+          const spotterSet = {};
+          let deleteAzn = false;
+          let deleteEby = false;
+          const result = results[index];
+          const id = result.custom_id.split("-")[1];
 
-      for (let index = 0; index < results.length; index++) {
-        const spotterSet = {};
-        let deleteAzn = false;
-        let deleteEby = false;
-        const result = results[index];
-        const id = result.custom_id.split("-")[1];
+          const product = products.find(
+            (product) => product._id.toString() === id
+          );
 
-        const product = products.find(
-          (product) => product._id.toString() === id
-        );
+          if (!product) continue;
 
-        if (!product) continue;
+          const { a_vrfd, e_vrfd } = product;
 
-        const { a_vrfd, e_vrfd } = product;
+          const content = result.response.body?.choices[0].message.content;
+          if (!content) continue;
 
-        const content = result.response.body?.choices[0].message.content;
-        if (!content) continue;
+          const update = safeJSONParse(content);
+          if (!update) continue;
 
-        const update = safeJSONParse(content);
-        if (!update) continue;
+          let nm_prop = "complete";
 
-        let nm_prop = "complete";
-
-        if ("a_score" && "a_isMatch" in update) {
-          const aScore = cleanScore(update.a_score);
-          if (aScore < MINIMAL_SCORE) {
-            deleteAzn = true;
-          } else if (!isNaN(aScore)) {
-            spotterSet["a_vrfd"] = {
-              ...a_vrfd,
-              nm_prop,
-              score: aScore,
-              isMatch: update.a_isMatch,
-            };
+          if ("a_score" && "a_isMatch" in update) {
+            const aScore = cleanScore(update.a_score);
+            if (aScore < MINIMAL_SCORE) {
+              deleteAzn = true;
+            } else if (!isNaN(aScore)) {
+              spotterSet["a_vrfd"] = {
+                ...a_vrfd,
+                nm_prop,
+                score: aScore,
+                isMatch: update.a_isMatch,
+              };
+            }
           }
-        }
 
-        if ("e_score" && "e_isMatch" in update) {
-          const eScore = cleanScore(update.e_score);
-          if (eScore < MINIMAL_SCORE) {
-            deleteEby = true;
-          } else if (!isNaN(eScore)) {
-            spotterSet["e_vrfd"] = {
-              ...e_vrfd,
-              nm_prop,
-              score: eScore,
-              isMatch: update.e_isMatch,
-            };
+          if ("e_score" && "e_isMatch" in update) {
+            const eScore = cleanScore(update.e_score);
+            if (eScore < MINIMAL_SCORE) {
+              deleteEby = true;
+            } else if (!isNaN(eScore)) {
+              spotterSet["e_vrfd"] = {
+                ...e_vrfd,
+                nm_prop,
+                score: eScore,
+                isMatch: update.e_isMatch,
+              };
+            }
           }
-        }
 
-        let bulkUpdate = {
-          updateOne: {
-            filter: { _id: new ObjectId(id) },
-            update: {
-              $unset: { nm_batchId: "", nm_prop: "" },
+          let bulkUpdate = {
+            updateOne: {
+              filter: { _id: new ObjectId(id) },
+              update: {
+                $unset: { nm_batchId: "", nm_prop: "" },
+              },
             },
-          },
-        };
-
-        if (Object.keys(spotterSet).length > 0) {
-          bulkUpdate.updateOne.update["$set"] = {
-            ...spotterSet,
-            nm_updatedAt: new Date().toISOString(),
           };
-        }
 
-        if (deleteAzn) {
-          const resetAzn = resetAznProductQuery();
-          bulkUpdate.updateOne.update["$unset"] = {
-            ...bulkUpdate.updateOne.update.$unset,
-            ...resetAzn.$unset,
-          };
-        }
+          if (Object.keys(spotterSet).length > 0) {
+            bulkUpdate.updateOne.update["$set"] = {
+              ...spotterSet,
+              nm_updatedAt: new Date().toISOString(),
+            };
+          }
 
-        if (deleteEby) {
-          const resetEby = resetEbyProductQuery();
-          bulkUpdate.updateOne.update["$unset"] = {
-            ...bulkUpdate.updateOne.update.$unset,
-            ...resetEby.$unset,
-          };
-        }
+          if (deleteAzn) {
+            const resetAzn = resetAznProductQuery();
+            bulkUpdate.updateOne.update["$unset"] = {
+              ...bulkUpdate.updateOne.update.$unset,
+              ...resetAzn.$unset,
+            };
+          }
 
-        bulkSpotterUpdates.push(bulkUpdate);
-      }
-      try {
-        await spotterDb.collection(shopDomain).bulkWrite(bulkSpotterUpdates);
-      } catch (error) {
-        console.error({
-          name: "Error updating spotterDb: ",
-          stack: `${error}`,
-        });
-        if (error instanceof MongoBulkWriteError) {
-          console.log({
-            name: "Mongo",
-            error: JSON.stringify(error.writeErrors, null, 2),
+          if (deleteEby) {
+            const resetEby = resetEbyProductQuery();
+            bulkUpdate.updateOne.update["$unset"] = {
+              ...bulkUpdate.updateOne.update.$unset,
+              ...resetEby.$unset,
+            };
+          }
+
+          bulkSpotterUpdates.push(bulkUpdate);
+        }
+        try {
+          await spotterDb.collection(shopDomain).bulkWrite(bulkSpotterUpdates);
+        } catch (error) {
+          console.error({
+            name: "Error updating spotterDb: ",
+            stack: `${error}`,
           });
+          if (error instanceof MongoBulkWriteError) {
+            console.log({
+              name: "Mongo",
+              error: JSON.stringify(error.writeErrors, null, 2),
+            });
+          }
         }
       }
     }),
