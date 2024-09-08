bdddbbe37645a35115ad4f458d8c3cefb680c1e9
MaxLiebsch <liebsch@dipmaxexport.com>
2024-09-08 08:52:21 +0200

    fix: sequenially update the products

--- src/util/quantities/processResultsForShops.js
+++ src/util/quantities/processResultsForShops.js
@@ -30,198 +30,191 @@ export const processResultsForShops = async (fileContents, batchData) => {
     }
     batchMap.get(shopDomain).push(result);
   });
+  for (const [shopDomain, results] of batchMap.entries()) {
+    const ids = results.map((result) => result.custom_id.split("-")[1]);
+    const products = await spotterDb
+      .collection(shopDomain)
+      .find({ _id: { $in: ids.map((id) => new ObjectId(id)) } })
+      .toArray();
 
-  await Promise.all([
-    batchMap.forEach(async (results, shopDomain) => {
-      const ids = results.map((result) => result.custom_id.split("-")[1]);
-      const products = await spotterDb
-        .collection(shopDomain)
-        .find({ _id: { $in: ids.map((id) => new ObjectId(id)) } })
-        .toArray();
+    if (products.length) {
+      for (let index = 0; index < results.length; index++) {
+        const set = {};
+        const spotterSet = {};
+        const result = results[index];
+        const id = result.custom_id.split("-")[1];
 
-      
+        const product = products.find(
+          (product) => product._id.toString() === id
+        );
 
-      if (products.length) {
-        for (let index = 0; index < results.length; index++) {
-          const set = {};
-          const spotterSet = {};
-          const result = results[index];
-          const id = result.custom_id.split("-")[1];
-
-          const product = products.find(
-            (product) => product._id.toString() === id
-          );
-
-          if (!product) {
-            console.error({
-              name: "Product not found in spotterDb",
-              id,
-              shopDomain,
-            });
-            continue;
-          }
-
-          const content = result.response.body?.choices[0].message.content;
-          const {
-            e_prc: eSellPrice,
-            prc: buyPrice,
-            a_prc: aSellPrice,
-            ebyCategories,
-            costs,
-            a_vrfd,
-            e_vrfd,
-          } = product;
-
-          if (!content) continue;
-
-          const update = safeJSONParse(content);
-          if (!update) continue;
-
-          Object.entries(update).forEach(([key, value]) => {
-            let qty = Number(value);
-            if (qty) {
-              if (qty === 0) qty = 1;
-              if (key === "a_nm") {
-                set["a_qty"] = qty;
-              }
-              if (key === "e_nm") {
-                set["e_qty"] = qty;
-              }
-              if (key === "nm") {
-                set["qty"] = qty;
-              }
-            }
+        if (!product) {
+          console.error({
+            name: "Product not found in spotterDb",
+            id,
+            shopDomain,
           });
+          continue;
+        }
 
-          const { a_qty: aSellQty, e_qty: eSellQty, qty: buyQty } = set;
+        const content = result.response.body?.choices[0].message.content;
+        const {
+          e_prc: eSellPrice,
+          prc: buyPrice,
+          a_prc: aSellPrice,
+          ebyCategories,
+          costs,
+          a_vrfd,
+          e_vrfd,
+        } = product;
 
-          if (buyQty && buyQty > 0) {
-            spotterSet["uprc"] = roundToTwoDecimals(product.prc / buyQty);
-          } else {
-            spotterSet["uprc"] = product.prc;
-            set["qty"] = 1;
-          }
-          let qty_prop = "complete";
-          if ("nm_score" in update) {
-            const nmScore = cleanScore(update.nm_score);
-            if (nmScore < MINIMAL_QUANTITY_SCORE) {
-              //to low score
-            } else if (!isNaN(nmScore)) {
-              spotterSet["nm_vrfd"] = {
-                qty_prop,
-                qty_score: nmScore,
-              };
+        if (!content) continue;
+
+        const update = safeJSONParse(content);
+        if (!update) continue;
+
+        Object.entries(update).forEach(([key, value]) => {
+          let qty = Number(value);
+          if (qty) {
+            if (qty === 0) qty = 1;
+            if (key === "a_nm") {
+              set["a_qty"] = qty;
+            }
+            if (key === "e_nm") {
+              set["e_qty"] = qty;
+            }
+            if (key === "nm") {
+              set["qty"] = qty;
             }
           }
+        });
 
-          if ("a_score" in update) {
-            const aScore = cleanScore(update.a_score);
-            if (aScore < MINIMAL_QUANTITY_SCORE) {
-              // score too bad
-            } else if (!isNaN(aScore)) {
-              if (aSellQty && aSellPrice && costs && aSellQty > 0) {
-                spotterSet["a_uprc"] = roundToTwoDecimals(
-                  aSellPrice / aSellQty
-                );
+        const { a_qty: aSellQty, e_qty: eSellQty, qty: buyQty } = set;
 
-                const factor = aSellQty / buyQty;
-                const arbitrage = calculateAznArbitrage(
-                  buyPrice * factor, // prc * (a_qty / qty), // EK
-                  aSellPrice, // a_prc, // VK
-                  product.costs,
-                  product?.tax
+        if (buyQty && buyQty > 0) {
+          spotterSet["uprc"] = roundToTwoDecimals(product.prc / buyQty);
+        } else {
+          spotterSet["uprc"] = product.prc;
+          set["qty"] = 1;
+        }
+        let qty_prop = "complete";
+        if ("nm_score" in update) {
+          const nmScore = cleanScore(update.nm_score);
+          if (nmScore < MINIMAL_QUANTITY_SCORE) {
+            //to low score
+          } else if (!isNaN(nmScore)) {
+            spotterSet["nm_vrfd"] = {
+              qty_prop,
+              qty_score: nmScore,
+            };
+          }
+        }
+
+        if ("a_score" in update) {
+          const aScore = cleanScore(update.a_score);
+          if (aScore < MINIMAL_QUANTITY_SCORE) {
+            // score too bad
+          } else if (!isNaN(aScore)) {
+            if (aSellQty && aSellPrice && costs && aSellQty > 0) {
+              spotterSet["a_uprc"] = roundToTwoDecimals(aSellPrice / aSellQty);
+
+              const factor = aSellQty / buyQty;
+              const arbitrage = calculateAznArbitrage(
+                buyPrice * factor, // prc * (a_qty / qty), // EK
+                aSellPrice, // a_prc, // VK
+                product.costs,
+                product?.tax
+              );
+              Object.entries(arbitrage).forEach(([key, value]) => {
+                spotterSet[key] = value;
+              });
+            }
+            spotterSet["a_vrfd"] = {
+              ...a_vrfd,
+              qty_prop,
+              qty_score: aScore,
+            };
+          }
+        }
+
+        if ("e_score" in update) {
+          const eScore = cleanScore(update.e_score);
+          if (eScore < MINIMAL_QUANTITY_SCORE) {
+            // score too bad
+          } else if (!isNaN(eScore)) {
+            if (
+              eSellQty &&
+              eSellPrice &&
+              ebyCategories?.length > 0 &&
+              eSellQty > 0
+            ) {
+              spotterSet["e_uprc"] = roundToTwoDecimals(
+                product.e_prc / eSellQty
+              );
+              const mappedCategories = findMappedCategory(
+                product.ebyCategories.reduce((acc, curr) => {
+                  acc.push(curr.id);
+                  return acc;
+                }, [])
+              );
+              const factor = eSellQty / buyQty;
+              if (mappedCategories) {
+                const arbitrage = calculateEbyArbitrage(
+                  mappedCategories,
+                  eSellPrice, //VK
+                  buyPrice * factor // prc * (e_qty / qty) //EK  //QTY Zielshop/QTY Herkunftsshop
                 );
-                Object.entries(arbitrage).forEach(([key, value]) => {
-                  spotterSet[key] = value;
-                });
+                if (arbitrage)
+                  Object.entries(arbitrage).forEach(([key, value]) => {
+                    spotterSet[key] = value;
+                  });
               }
-              spotterSet["a_vrfd"] = {
-                ...a_vrfd,
-                qty_prop,
-                qty_score: aScore,
-              };
             }
+            spotterSet["e_vrfd"] = {
+              ...e_vrfd,
+              qty_prop,
+              qty_score: eScore,
+            };
           }
-
-          if ("e_score" in update) {
-            const eScore = cleanScore(update.e_score);
-            if (eScore < MINIMAL_QUANTITY_SCORE) {
-              // score too bad
-            } else if (!isNaN(eScore)) {
-              if (
-                eSellQty &&
-                eSellPrice &&
-                ebyCategories?.length > 0 &&
-                eSellQty > 0
-              ) {
-                spotterSet["e_uprc"] = roundToTwoDecimals(
-                  product.e_prc / eSellQty
-                );
-                const mappedCategories = findMappedCategory(
-                  product.ebyCategories.reduce((acc, curr) => {
-                    acc.push(curr.id);
-                    return acc;
-                  }, [])
-                );
-                const factor = eSellQty / buyQty;
-                if (mappedCategories) {
-                  const arbitrage = calculateEbyArbitrage(
-                    mappedCategories,
-                    eSellPrice, //VK
-                    buyPrice * factor // prc * (e_qty / qty) //EK  //QTY Zielshop/QTY Herkunftsshop
-                  );
-                  if (arbitrage)
-                    Object.entries(arbitrage).forEach(([key, value]) => {
-                      spotterSet[key] = value;
-                    });
-                }
-              }
-              spotterSet["e_vrfd"] = {
-                ...e_vrfd,
-                qty_prop,
-                qty_score: eScore,
-              };
-            }
-          }
-          bulkSpotterUpdates.push({
-            updateOne: {
-              filter: { _id: new ObjectId(id) },
-              update: {
-                $set: {
-                  ...set,
-                  ...spotterSet,
-                  qty_updatedAt: new Date().toISOString(),
-                },
-                $unset: {
-                  qty_batchId: "",
-                  qty_prop: "",
-                },
+        }
+        bulkSpotterUpdates.push({
+          updateOne: {
+            filter: { _id: new ObjectId(id) },
+            update: {
+              $set: {
+                ...set,
+                ...spotterSet,
+                qty_updatedAt: new Date().toISOString(),
+              },
+              $unset: {
+                qty_batchId: "",
+                qty_prop: "",
               },
             },
-          });
-        }
-        try {
-          await spotterDb.collection(shopDomain).bulkWrite(bulkSpotterUpdates);
-        } catch (error) {
-          console.error({
-            name: "Error updating spotterDb: ",
-            stack: `${error}`,
-          });
-          if (error instanceof MongoBulkWriteError) {
-            console.log({
-              name: "Mongo",
-              error: JSON.stringify(error.writeErrors, null, 2),
-            });
-          }
-        }
-      }else{
-        console.error({
-          name: "No products found in spotterDb",
-          shopDomain,
+          },
         });
       }
-    }),
-  ]);
+      try {
+        await spotterDb.collection(shopDomain).bulkWrite(bulkSpotterUpdates);
+      } catch (error) {
+        console.error({
+          name: "Error updating spotterDb: ",
+          stack: `${error}`,
+        });
+        if (error instanceof MongoBulkWriteError) {
+          console.log({
+            name: "Mongo",
+            error: JSON.stringify(error.writeErrors, null, 2),
+          });
+        }
+      }
+    } else {
+      console.error({
+        name: "No products found in spotterDb",
+        shopDomain,
+      });
+    }
+  }
 
   const tasksCol = crawlDataDb.collection("tasks");
   await tasksCol.updateOne(
--- src/util/titles/processResultsForShops.js
+++ src/util/titles/processResultsForShops.js
@@ -47,129 +47,127 @@ export const processResultsForShops = async (fileContents, batchData) => {
     batchMap.get(shopDomain).push(result);
   });
 
-  await Promise.all([
-    batchMap.forEach(async (results, shopDomain) => {
-      const ids = results.map((result) => result.custom_id.split("-")[1]);
-      const products = await spotterDb
-        .collection(shopDomain)
-        .find({ _id: { $in: ids.map((id) => new ObjectId(id)) } })
-        .toArray();
+  for (const [shopDomain, results] of batchMap.entries()) {
+    const ids = results.map((result) => result.custom_id.split("-")[1]);
+    const products = await spotterDb
+      .collection(shopDomain)
+      .find({ _id: { $in: ids.map((id) => new ObjectId(id)) } })
+      .toArray();
 
-      if (products.length) {
-        for (let index = 0; index < results.length; index++) {
-          const spotterSet = {};
-          let deleteAzn = false;
-          let deleteEby = false;
-          const result = results[index];
-          const id = result.custom_id.split("-")[1];
+    if (products.length) {
+      for (let index = 0; index < results.length; index++) {
+        const spotterSet = {};
+        let deleteAzn = false;
+        let deleteEby = false;
+        const result = results[index];
+        const id = result.custom_id.split("-")[1];
 
-          const product = products.find(
-            (product) => product._id.toString() === id
-          );
+        const product = products.find(
+          (product) => product._id.toString() === id
+        );
 
-          if (!product) {
-            console.error({
-              name: "Product not found in spotterDb",
-              id,
-              shopDomain,
-            });
-            continue;
-          }
-
-          const { a_vrfd, e_vrfd } = product;
-
-          const content = result.response.body?.choices[0].message.content;
-          if (!content) continue;
-
-          const update = safeJSONParse(content);
-          if (!update) continue;
-
-          let nm_prop = "complete";
-
-          if ("a_score" in update && "a_isMatch" in update) {
-            const aScore = cleanScore(update.a_score);
-            if (aScore < MINIMAL_SCORE) {
-              deleteAzn = true;
-            } else if (!isNaN(aScore)) {
-              spotterSet["a_vrfd"] = {
-                ...a_vrfd,
-                nm_prop,
-                score: aScore,
-                isMatch: update.a_isMatch,
-              };
-            }
-          }
-
-          if ("e_score" in update && "e_isMatch" in update) {
-            const eScore = cleanScore(update.e_score);
-            if (eScore < MINIMAL_SCORE) {
-              deleteEby = true;
-            } else if (!isNaN(eScore)) {
-              spotterSet["e_vrfd"] = {
-                ...e_vrfd,
-                nm_prop,
-                score: eScore,
-                isMatch: update.e_isMatch,
-              };
-            }
-          }
-
-          let bulkUpdate = {
-            updateOne: {
-              filter: { _id: new ObjectId(id) },
-              update: {
-                $unset: { nm_batchId: "", nm_prop: "" },
-              },
-            },
-          };
-
-          if (Object.keys(spotterSet).length > 0) {
-            bulkUpdate.updateOne.update["$set"] = {
-              ...spotterSet,
-              nm_updatedAt: new Date().toISOString(),
-            };
-          }
-
-          if (deleteAzn) {
-            const resetAzn = resetAznProductQuery();
-            bulkUpdate.updateOne.update["$unset"] = {
-              ...bulkUpdate.updateOne.update.$unset,
-              ...resetAzn.$unset,
-            };
-          }
-
-          if (deleteEby) {
-            const resetEby = resetEbyProductQuery();
-            bulkUpdate.updateOne.update["$unset"] = {
-              ...bulkUpdate.updateOne.update.$unset,
-              ...resetEby.$unset,
-            };
-          }
-
-          bulkSpotterUpdates.push(bulkUpdate);
-        }
-        try {
-          await spotterDb.collection(shopDomain).bulkWrite(bulkSpotterUpdates);
-        } catch (error) {
+        if (!product) {
           console.error({
-            name: "Error updating spotterDb: ",
-            stack: `${error}`,
+            name: "Product not found in spotterDb",
+            id,
+            shopDomain,
           });
-          if (error instanceof MongoBulkWriteError) {
-            console.log({
-              name: "Mongo",
-              error: JSON.stringify(error.writeErrors, null, 2),
-            });
+          continue;
+        }
+
+        const { a_vrfd, e_vrfd } = product;
+
+        const content = result.response.body?.choices[0].message.content;
+        if (!content) continue;
+
+        const update = safeJSONParse(content);
+        if (!update) continue;
+
+        let nm_prop = "complete";
+
+        if ("a_score" in update && "a_isMatch" in update) {
+          const aScore = cleanScore(update.a_score);
+          if (aScore < MINIMAL_SCORE) {
+            deleteAzn = true;
+          } else if (!isNaN(aScore)) {
+            spotterSet["a_vrfd"] = {
+              ...a_vrfd,
+              nm_prop,
+              score: aScore,
+              isMatch: update.a_isMatch,
+            };
           }
         }
-      }else{
-        console.error({
-          name: "No products found in spotterDb",
-          shopDomain,
-        });
+
+        if ("e_score" in update && "e_isMatch" in update) {
+          const eScore = cleanScore(update.e_score);
+          if (eScore < MINIMAL_SCORE) {
+            deleteEby = true;
+          } else if (!isNaN(eScore)) {
+            spotterSet["e_vrfd"] = {
+              ...e_vrfd,
+              nm_prop,
+              score: eScore,
+              isMatch: update.e_isMatch,
+            };
+          }
+        }
+
+        let bulkUpdate = {
+          updateOne: {
+            filter: { _id: new ObjectId(id) },
+            update: {
+              $unset: { nm_batchId: "", nm_prop: "" },
+            },
+          },
+        };
+
+        if (Object.keys(spotterSet).length > 0) {
+          bulkUpdate.updateOne.update["$set"] = {
+            ...spotterSet,
+            nm_updatedAt: new Date().toISOString(),
+          };
+        }
+
+        if (deleteAzn) {
+          const resetAzn = resetAznProductQuery();
+          bulkUpdate.updateOne.update["$unset"] = {
+            ...bulkUpdate.updateOne.update.$unset,
+            ...resetAzn.$unset,
+          };
+        }
+
+        if (deleteEby) {
+          const resetEby = resetEbyProductQuery();
+          bulkUpdate.updateOne.update["$unset"] = {
+            ...bulkUpdate.updateOne.update.$unset,
+            ...resetEby.$unset,
+          };
+        }
+
+        bulkSpotterUpdates.push(bulkUpdate);
       }
-    }),
-  ]);
+      try {
+        await spotterDb.collection(shopDomain).bulkWrite(bulkSpotterUpdates);
+      } catch (error) {
+        console.error({
+          name: "Error updating spotterDb: ",
+          stack: `${error}`,
+        });
+        if (error instanceof MongoBulkWriteError) {
+          console.error({
+            name: "Mongo",
+            error: JSON.stringify(error.writeErrors, null, 2),
+          });
+        }
+      }
+    } else {
+      console.error({
+        name: "No products found in spotterDb",
+        shopDomain,
+      });
+    }
+  }
 
   const tasksCol = crawlDataDb.collection("tasks");
   await tasksCol.updateOne(
