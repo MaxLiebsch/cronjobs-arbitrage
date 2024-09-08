45cf988e56f2c805b4138463e44bbdf9667ab733
MaxLiebsch <liebsch@dipmaxexport.com>
2024-09-08 10:03:54 +0200

    Update testNamingPrompt.js

--- __test__/testNamingPrompt.js
+++ __test__/testNamingPrompt.js
@@ -43,53 +43,6 @@ export const testNamingPrompt = async () => {
     const tokenCnt = encodeChat(prompt.body.messages, "gpt-3.5-turbo").length;
     console.log('tokenCnt:', tokenCnt)
 
-  // console.log(products.length);
-  // const prompts = [];
-  // let tokens = 0;
-  // let cnt = 0;
-  // for (const product of products) {
-
-  //   if (tokens + tokenCnt < 3000) {
-  //     tokens += tokenCnt;
-  //     cnt++;
-  //     prompts.push(prompt);
-  //   } else {
-  //     break;
-  //   }
-  // }
-  // console.log("prompts:", JSON.stringify(prompts[0], null, 2));
-
-  // console.log("cnt: ", cnt, "Tokens: ", tokens);
-
-  try {
-    // const filepath = await createJsonlFile(prompts);
-    // console.log("filepath:", filepath);
-    // const file = await openai.files.create({
-    //   file: createReadStream(filepath),
-    //   purpose: "batch",
-    // });
-    // console.log(file); //file.id
-    // const batch = await openai.batches.create({
-    //   input_file_id: file.id,
-    //   endpoint: "/v1/chat/completions",
-    //   completion_window: "24h",
-    // });
-    // console.log(batch); // batch.id
-    // const batch = await openai.batches.retrieve('batch_yGT3DVu5wM6BtgrdVhQtHiel');
-    // console.log('batch:', batch)
-    // const fileResponse = await openai.files.content(
-    //   "file-tsEj4QUJ382ztYPN3wPDsFsn"
-    // );
-    // const fileContents = await fileResponse.text();
-    // console.log(
-    //   JSON.parse(
-    //     fileContents.split("\n").filter(Boolean).map(JSON.parse)[0].response
-    //       .body.choices[0].message.content
-    //   )
-    // );
-  } catch (error) {
-    console.log("error:", error);
-  }
 };
 
 testNamingPrompt().then(() => {
